b4bef8afd05d0ec506e6540ff2fab351f5b9b23d
deribew <deribewsoftware@gmail.com>
2024-01-08 11:27:51 +0300

    :updated

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/deribewsoftware/mongo_golang_rest/controllers"
 	"github.com/julienschmidt/httprouter"
 	"gopkg.in/mgo.v2"
+	
 )
 func main() {
 	r:=httprouter.New()
-	uc :=controllers.NewUserController(getsession())
-	r.GET("",)
-	r.POST("/",)
-	r.DELETE("/")
+	uc :=controllers.NewUserController(getSession())
+	r.GET("user/:id",uc.GetUser)
+	r.POST("/user",uc.CreateUser)
+	r.DELETE("/user:id",uc.DeleteUser)
+	http.ListenAndServe("localhost:9000",r)
 
 }
 
-func getSession()*mgo.Session{
+func getSession() *mgo.Session{
+	s,err:=mgo.Dial("mongodb+srv://deribewtech:golang_rest@cluster0.pbdh8ir.mongodb.net")
+	if err != nil{
+		panic(err)
+	}
+	return s
 
 }
